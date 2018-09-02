c66fff546b3e2ac2a8fa38efe7ac854c21c63bfa
Theofilos Chamalis <theofxam@gmail.com>
2018-09-02 13:11:20 +0300

    Created profile actions and reducers and minimal UI logic

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import store                              from './store';
 import jwt_decode                         from 'jwt-decode';
 import setAuthToken                       from './utils/setAuthToken';
 import { setCurrentUser, logoutUser }     from './actions/authActions';
+import { clearCurrentProfile }            from './actions/profileActions';
 
-import Navbar   from './components/layout/Navbar';
-import Footer   from './components/layout/Footer';
-import Landing  from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login    from './components/auth/Login';
+import Navbar    from './components/layout/Navbar';
+import Footer    from './components/layout/Footer';
+import Landing   from './components/layout/Landing';
+import Register  from './components/auth/Register';
+import Login     from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -29,7 +31,8 @@ if ( token ) {
     if ( decoded.exp < currentTime ) {
         // Logout the user
         store.dispatch ( logoutUser () );
-        // TODO: Clear current profile
+        // Clear current profile
+        store.dispatch ( clearCurrentProfile () );
         // Redirect to login
         window.location.href = '/login';
     }
@@ -46,6 +49,7 @@ class App extends Component {
                         <div className = 'container'>
                             <Route exact path = '/register' component = { Register }/>
                             <Route exact path = '/login' component = { Login }/>
+                            <Route exact path = '/dashboard' component = { Dashboard }/>
                         </div>
                         <Footer/>
                     </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import { Link }             from 'react-router-dom';
-import PropTypes            from 'prop-types';
-import { connect }          from 'react-redux';
-import { logoutUser }       from '../../actions/authActions';
+import React, { Component }    from 'react';
+import { Link }                from 'react-router-dom';
+import PropTypes               from 'prop-types';
+import { connect }             from 'react-redux';
+import { logoutUser }          from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
     onLogoutClick = ( e ) => {
         e.preventDefault ();
+        this.props.clearCurrentProfile ();
         this.props.logoutUser ();
         // Cannot use history here, bc this is not a Route
         window.location.href = '/';
@@ -75,4 +77,4 @@ const mapStateToProps = ( state ) => ( {
     auth : state.auth
 } );
 
-export default connect ( mapStateToProps, { logoutUser } ) ( Navbar );
+export default connect ( mapStateToProps, { logoutUser, clearCurrentProfile } ) ( Navbar );
