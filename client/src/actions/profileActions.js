8d7cee2914b21465078bf89672f666d514d6faf2
Theofilos Chamalis <theofxam@gmail.com>
2018-09-06 22:23:46 +0300

    Display Education and Experience in Dashboard, React updated to latest version

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -51,6 +51,32 @@ export const addEducation = ( eduData, history ) => ( dispatch ) => {
         } ) );
 };
 
+// Delete experience
+export const deleteExperience = ( id ) => ( dispatch ) => {
+    axios.delete ( `/api/profile/experience/${id}` )
+        .then ( res => dispatch ( {
+            type : GET_PROFILE,
+            payload : res.data
+        } ) )
+        .catch ( err => dispatch ( {
+            type : GET_ERRORS,
+            payload : err.response.data
+        } ) );
+};
+
+// Delete education
+export const deleteEducation = ( id ) => ( dispatch ) => {
+    axios.delete ( `/api/profile/education/${id}` )
+        .then ( res => dispatch ( {
+            type : GET_PROFILE,
+            payload : res.data
+        } ) )
+        .catch ( err => dispatch ( {
+            type : GET_ERRORS,
+            payload : err.response.data
+        } ) );
+};
+
 // Delete Account & Profile
 export const deleteAccount = () => ( dispatch ) => {
     if ( window.confirm ( `Are you sure? \nThis action can NOT be undone!` ) ) {
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { connect }                          from 'react-redux';
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner                              from '../common/spinner';
 import ProfileActions                       from './ProfileActions';
+import Experience                           from './Experience';
+import Education                            from './Education';
 
 class Dashboard extends Component {
     componentDidMount () {
@@ -31,7 +33,8 @@ class Dashboard extends Component {
                         <p className = 'lead text-muted'>Welcome <Link to = { `/profile/${profile.handle}` }>{ user.name }</Link>
                         </p>
                         <ProfileActions/>
-                        { /* TODO: exp and education*/ }
+                        <Experience experience = { profile.experience }/>
+                        <Education education = { profile.education }/>
                         <div style = { { marginBottom : '60px' } }>
                             <button onClick = { this.onDeleteClick } className = 'btn btn-danger'>Delete My Account
                             </button>
