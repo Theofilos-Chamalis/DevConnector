67fefcf2ffaffc3a685bf6d15337d5f6c134f947
Theofilos Chamalis <theofxam@gmail.com>
2018-11-04 19:48:35 +0200

    Add npm package to log redux only in production to avoid mobile phone errors

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'; // Use the npm package to avoid errors on mobile browsers
 import rootReducer from './reducers'; // We don't need to specify index.js here as it is the default
 
 const initialState = {};
 
 const middleware = [thunk];
 
-// Reducer, preloaded state, enhancer(middleware)
-// Add the chrome extension redux devtools middleware here
 const store = createStore(
 	rootReducer,
 	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
