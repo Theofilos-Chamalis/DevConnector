6da3d5287f6a1315c9ae1a5e620a1821f038514b
Theofilos <t.chamalis@vieconnect.io>
2020-08-23 14:26:01 +0300

    Added new dev keys for Mongo Atlas and keep old MLAB Mongo keys as well

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,6 @@
+// These credentials were created only for development purposes
 module.exports = {
-	mongoURI: 'mongodb://theo:tsoba405012@ds145750.mlab.com:45750/dev_connector',
+	oldMLABMongoURI: 'mongodb://theo:tsoba405012@ds145750.mlab.com:45750/dev_connector',
+	mongoURI: 'mongodb+srv://theo:tsoba405012@dev-connector.bytbr.mongodb.net/dev_connector?retryWrites=true&w=majority',
 	secretOrKey: 'whatever'
 };
